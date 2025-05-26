31f4589396bfdc22b4d7021cd860f7f13e20e1b3
manofsky <clegrof@gmail.com>
2025-05-27 00:27:50 +0300

    feat: add query type

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -135,3 +135,77 @@ const UserType = new GraphQLObjectType<User, GqlContext>({
     },
   }),
 });
+
+const QueryType = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: () => ({
+    memberTypes: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(MemberType))),
+      resolve: async (_source, _args, context: GqlContext) => {
+        return context.prisma.memberType.findMany();
+      },
+    },
+    memberType: {
+      type: MemberType,
+      args: {
+        id: { type: new GraphQLNonNull(MemberTypeIdEnum) },
+      },
+      resolve: async (_source, args: { id: MemberTypeId }, context: GqlContext) => {
+        return context.prisma.memberType.findUnique({
+          where: { id: args.id },
+        });
+      },
+    },
+    users: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))),
+      resolve: async (_source, _args, context: GqlContext) => {
+        return context.prisma.user.findMany();
+      },
+    },
+    user: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+      resolve: async (_source, args: { id: string }, context: GqlContext) => {
+        return context.prisma.user.findUnique({
+          where: { id: args.id },
+        });
+      },
+    },
+    posts: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(PostType))),
+      resolve: async (_source, _args, context: GqlContext) => {
+        return context.prisma.post.findMany();
+      },
+    },
+    post: {
+      type: PostType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+      resolve: async (_source, args: { id: string }, context: GqlContext) => {
+        return context.prisma.post.findUnique({
+          where: { id: args.id },
+        });
+      },
+    },
+    profiles: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(ProfileType))),
+      resolve: async (_source, _args, context: GqlContext) => {
+        return context.prisma.profile.findMany();
+      },
+    },
+    profile: {
+      type: ProfileType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+      resolve: async (_source, args: { id: string }, context: GqlContext) => {
+        return context.prisma.profile.findUnique({
+          where: { id: args.id },
+        });
+      },
+    },
+  }),
+});
